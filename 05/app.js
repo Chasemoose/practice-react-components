473ff8e62bfb5bdbded3fdb5cdeac1fd2ccc3c64
Jarek Stelmach <j.stelmach@onet.eu>
2024-08-06 22:42:50 +0200

    Added some additional code to componentDidMount

--- 05/app.js
+++ 05/app.js
@@ -13,6 +13,24 @@ class weather extends React.Component {
 
     componentDidMount() {
         const {lat, lon, apiKey} = this.props
+
+        fetch(`https://api.weatherbit.io/v2.0/current?key=${apiKey}&lat=${lat}&lon=${lon}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                data: data.data[0],
+                loading: false,
+            })
+        })
+        .catch(error => {
+            this.setState({
+                error,
+                loading: false,
+            })
+        })
+
+
+
     }
 
 
